c6238b401ec07aa7704c11eaab9a8578ed80101f
Elijah Melton <elimelt@uw.edu>
2025-08-10 01:43:43 -0700

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-…  base: './',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
